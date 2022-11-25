3bee8e5bbab975684c40c284390751b09216f2ac
johnschatner <leontin2001@gmail.com>
2022-11-25 14:30:10 +0100

    Update 2
    Make dropdown dynamic
    
    Add helper.js
    
    Add relevant fetch functions
    
    Add complementary API for images
    
    Add function to search akabab API for matching objects to get image url and name, cross referencing to SWAPI search

--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -1,6 +1,9 @@
+// Global variables
 const swapiUrl = "https://swapi.dev/api/";
+const akababUrl = "https://akabab.github.io/starwars-api/api/all.json";
 const searchParameter = "?search=";
 
+// Get response from SWAPI
 function getSwapiData() {
   let selectValue = document.querySelector("#listSelector").value;
   let searchValue = document.querySelector("#searchField").value;
@@ -12,5 +15,41 @@ function getSwapiData() {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      checkIfPeople();
     });
+}
+
+// Get response from akabab
+async function getAkababData(akababUrl) {
+  const response = await fetch(akababUrl);
+  const data = await response.json();
+  return data;
+}
+// Run getSwapiData func
+function checkIfPeople() {
+  let selectValue = document.querySelector("#listSelector").value;
+  console.log(selectValue);
+  if (selectValue == "people") {
+    getAkababData(akababUrl).then((data) => {
+      searchAkabab(data);
+    });
+  } else {
+    console.log("(❁´◡`❁)");
+  }
+}
+
+function searchAkabab(data) {
+  console.log(data);
+  let searchValue = document.querySelector("#searchField").value;
+  searchValue = searchValue.toLowerCase();
+  const persons = []; // Create new object to store "Name" and "Image"
+
+  for (let i = 0; i < data.length; i++) {
+    const person = {
+      name: data[i].name.toLowerCase(),
+      image: data[i].image,
+    };
+    persons.push(person);
+  }
+  console.log(filterIt(persons, searchValue));
 }
