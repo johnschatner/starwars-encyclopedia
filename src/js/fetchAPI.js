092eac7a8b8c2e9e6cf6991751d1df7ada9680c0
johnschatner <leontin2001@gmail.com>
2022-11-30 10:26:35 +0100

    Almost working modal identification

--- src/js/fetchAPI.js
+++ src/js/fetchAPI.js
@@ -12,10 +12,6 @@ function displayLoading() {
   loader.classList.add("text-yellow-300");
   //Enables pointer-events-none so inputs cant be made while loading occurs
   searchBar.classList.add("pointer-events-none");
-  // to stop loading after some time
-  setTimeout(() => {
-    loader.classList.remove("display");
-  }, 5000);
 }
 // hide loading
 function hideLoading() {
@@ -167,6 +163,8 @@ function renderSearchResults(data) {
   let searchResults = document.querySelector(".search-results");
   searchResults.classList.remove("hidden");
   const btnNode = document.querySelector(".search-result");
+  btnNode.addEventListener("click", renderModal);
+  btnNode.data = data.results[1];
 
   searchResults.innerHTML = ""; // Clear previous search results
   searchResults.appendChild(btnNode); // Clear previous search results
@@ -175,6 +173,9 @@ function renderSearchResults(data) {
   for (let j = 0; j < data.count; j++) {
     // Append the amount of search results
     let btnNodeClone = btnNode.cloneNode(true); // clone a new search result
+    btnNodeClone.setAttribute("id", `clone${j}`);
+    btnNodeClone.addEventListener("click", renderModal, false);
+    btnNodeClone.data = data.results[j - 1]; // assign the API response to the button
     searchResults.appendChild(btnNodeClone); // append a new search result
 
     // Get each span tag with applicable class (key || value)
@@ -198,3 +199,7 @@ function renderSearchResults(data) {
 
 // CLEAR SEARCH RESULT AFTER EACH SEARCH [DONE]
 // LINE: 167-168
+function renderModal(data) {
+  console.log("rendering modal");
+  console.log(data.currentTarget.data);
+}
